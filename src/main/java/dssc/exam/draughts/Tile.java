778a75d44e306289d70ed3a59da7881648459a02
mibbim <michele.berti.g@gmail.com>
2022-02-04 11:29:27 +0100

    We now create a Tile containing a Piece

---
+++ src/main/java/dssc/exam/draughts/Tile.java
@@ -0,0 +1,9 @@
+package dssc.exam.draughts;
+
+public class Tile {
+     Piece piece;
+
+    Tile(Piece piece) {
+        this.piece = piece;
+    }
+}
---
+++ src/test/java/dssc/exam/draughts/TestTile.java
@@ -0,0 +1,16 @@
+package dssc.exam.draughts;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestTile {
+
+    @Test
+    void createNonEmptyTile() {
+        Piece piece = new Piece(1, Color.BLACK);
+        Tile tile = new Tile(piece);
+        assertEquals(tile.piece, piece);
+    }
+
+}
