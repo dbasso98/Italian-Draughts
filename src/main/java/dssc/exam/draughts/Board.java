320abe554a1287a35a42578286aa69759da692ef
dbasso98 <dav.basso98@gmail.com>
2022-02-05 11:26:48 +0100

    First draft for Board class, added Tile methods and members

---
+++ src/main/java/dssc/exam/draughts/Board.java
@@ -0,0 +1,10 @@
+package dssc.exam.draughts;
+
+import java.util.ArrayList;
+
+public class Board {
+    ArrayList<Tile> board = new ArrayList<Tile>(64);
+    Board() {
+
+    }
+}
--- src/main/java/dssc/exam/draughts/Tile.java
+++ src/main/java/dssc/exam/draughts/Tile.java
@@ -1,16 +1,41 @@
 package dssc.exam.draughts;
 
+import java.awt.*;
+
 public class Tile {
     Piece piece = null;
+    Point coords = null;
 
-    Tile(Piece piece) {
+    Tile(Piece piece, Point coords) {
+        this(coords);
         this.piece = piece;
+    }
+
+    Tile(Piece piece, int x, int y) {
+        this(x,y);
+        this.piece = piece;
+    }
+
+    Tile(Point coords) {
+        this.coords = coords;
+    }
+
+    Tile(int x, int y) {
+        this.coords.setLocation(x,y);
     }
 
     Tile() {
     }
 
-    boolean empty(){
+    boolean is_tile_empty() {
         return piece == null;
+    }
+
+    void reset_tile() {
+        this.piece = null;
+    }
+
+    void set_piece_contained_in_tile(Piece piece) {
+        this.piece = piece;
     }
 }
