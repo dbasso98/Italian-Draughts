776d40c20cff8e2f7503ef2f21f63705dfd3f538
mibbim <michele.berti.g@gmail.com>
2022-02-08 16:07:24 +0100

    Solved broken build

--- src/main/java/dssc/exam/draughts/Board.java
+++ src/main/java/dssc/exam/draughts/Board.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 
 public class Board {
     public final int size = 64;
+    public final int lastIndex = size - 1;
     public final int maxRows = 8;
     public final int maxColumns = 8;
     public final int piecesPerPlayer = 12;
@@ -25,10 +26,10 @@ public class Board {
                 board.add(new Tile(secondColor));
             }
         }
-        for (int i = 0; i < piecesPerPlayer * 2; ++i) {
-            if (board.get(i).getTileColor() == Color.BLACK) {
-                board.get(i).setPieceContainedInTile(new Piece(i, Color.BLACK));
-                board.get(size - 1 - i).setPieceContainedInTile(new Piece(size - 1 - i, Color.WHITE));
+        for (int tileIndex = 0; tileIndex < piecesPerPlayer * 2; ++tileIndex) {
+            if (board.get(tileIndex).getTileColor() == Color.BLACK) {
+                board.get(tileIndex).setPieceContainedInTile(new Piece(tileIndex, Color.BLACK));
+                board.get(lastIndex - tileIndex).setPieceContainedInTile(new Piece(lastIndex - tileIndex, Color.WHITE));
             }
         }
     }
@@ -43,8 +44,8 @@ public class Board {
 
     public int getPiecesOfColor(Color color) {
         int sum = 0;
-        for (int i = 0; i < size; ++i) {
-            if (board.get(i).isTileNotEmpty() && board.get(i).getTilePiece().getColorOfPiece() == color)
+        for (int tileIndex = 0; tileIndex < getSizeOfBoard(); ++tileIndex) {
+            if (board.get(tileIndex).isTileNotEmpty() && board.get(tileIndex).getTilePiece().getColorOfPiece() == color)
                 sum += 1;
         }
         return sum;
@@ -52,33 +53,44 @@ public class Board {
 
     public int getTotalNumberOfPieces() {
         int sum = 0;
-        for (int i = 0; i < size; ++i) {
-            if (board.get(i).isTileNotEmpty())
+        for (int tileIndex = 0; tileIndex < getSizeOfBoard(); ++tileIndex) {
+            if (board.get(tileIndex).isTileNotEmpty())
                 sum += 1;
         }
         return sum;
     }
 
-    public Tile getTile(int position) {
-        return board.get(position);
+    public Tile getTile(int index) {
+        return board.get(index);
+    }
+
+    public Tile getSymmetricTile(int index) {
+        return board.get(lastIndex - index);
+    }
+
+    public Tile getTile(int row, int column) {
+        return board.get(getIndex(row, column));
     }
 
     public Tile getSymmetricTile(int position) {
         return board.get(size - 1 - position);
     }
 
+    public int getMiddlePosition(int sourceRow, int sourceColumn, int destinationRow, int destinationColumn) {
+        return getMiddlePosition(getIndex(sourceRow, sourceColumn), getIndex(destinationRow, destinationColumn));
+
     public int getMiddlePosition(int startPosition, int endPosition) {
-        int distance = Math.abs(startPosition-endPosition);
+        int distance = Math.abs(startPosition - endPosition);
         if (!isValidPosition(startPosition) || !isValidPosition(endPosition)) {
             return -1;
         }
         if (getTile(startPosition).getTileColor() == Color.WHITE ||
-            getTile(endPosition).getTileColor() == Color.WHITE)
+                getTile(endPosition).getTileColor() == Color.WHITE)
             return -1;
         if (distance != 14 && distance != 18) {
             return -1;
         }
-        return Math.min(startPosition, endPosition) + distance/2;
+        return Math.min(startPosition, endPosition) + distance / 2;
     }
 
     public boolean isValidPosition(int position) {
@@ -88,7 +100,7 @@ public class Board {
     public void display() {
         for (int row = 0; row < maxRows; row++) {
             for (int col = 0; col < maxRows; col++) {
-                System.out.print(getTile(getIndex(row, col)).display());
+                System.out.print(getTile(row, col).display());
             }
             System.out.print("\n");
         }
