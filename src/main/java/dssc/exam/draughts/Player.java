c9e88faea7e494b13984e300aa3f71cb940f6640
mibbim <michele.berti.g@gmail.com>
2022-02-09 12:34:08 +0100

    Refactoring Player

--- src/main/java/dssc/exam/draughts/Player.java
+++ src/main/java/dssc/exam/draughts/Player.java
@@ -1,2 +1,23 @@
-package dssc.exam.draughts;public class Player {
+package dssc.exam.draughts;
+
+import javax.swing.text.Position;
+import java.awt.*;
+import java.util.Scanner;
+
+public class Player {
+
+    private static final String readMessage = "What are the coordinates (row, column) of the piece you intend to move? \n(e.g. 3 4)";
+
+    Point readPosition() {
+        return readPosition(readMessage);
+    }
+
+    Point readPosition(String message) {
+        System.out.println(message);
+        Scanner input = new Scanner(System.in);
+        int row = input.nextInt();
+        int column = input.nextInt();
+        return new Point(row, column);
+    }
+
 }
--- src/test/java/dssc/exam/draughts/TestPlayer.java
+++ src/test/java/dssc/exam/draughts/TestPlayer.java
@@ -1,2 +1,27 @@
-package dssc.exam.draughts;public class TestPlayer {
+package dssc.exam.draughts;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.awt.*;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestPlayer {
+
+    @ParameterizedTest
+    @CsvSource({"3, 4, 3, 4", "12, 15, 12, 15"})
+    void testReadPosition(String rowInput, String columnInput,
+                          int rowExpected, int columnExpected ) {
+        String fakeInput = rowInput + " " + columnInput + "\n";
+        ByteArrayInputStream fakeStandardInput = new ByteArrayInputStream(fakeInput.getBytes());
+        System.setIn(fakeStandardInput);
+
+        Player player = new Player();
+        Point point = player.readPosition();
+        assertEquals(point.x, rowExpected);
+        assertEquals(point.y, columnExpected);
+    }
 }
