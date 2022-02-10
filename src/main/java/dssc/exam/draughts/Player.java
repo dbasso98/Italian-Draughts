303bfa33bd062522398817f663354811212ea00d
mibbim <michele.berti.g@gmail.com>
2022-02-10 13:36:08 +0100

    We now raise exception if non numeric input is given

--- src/main/java/dssc/exam/draughts/Player.java
+++ src/main/java/dssc/exam/draughts/Player.java
@@ -1,12 +1,13 @@
 package dssc.exam.draughts;
 
 import java.awt.*;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Player {
     public final Color color;
-    private static final String readSourceMessage = "What are the coordinates (x, y) of the piece you intend to move? \n(e.g. 3 4)";
-    private static final String readDestinationMessage = "What are the coordinates (x, y) of the Tile you intend to move the piece to? \n(e.g. 3 4)";
+    private static final String readSourceMessage = "What are the coordinates (x, y) of the piece you intend to move? (e.g. 3 4)";
+    private static final String readDestinationMessage = "What are the coordinates (x, y) of the Tile you intend to move the piece to? (e.g. 3 4)";
     private final Scanner input = new Scanner(System.in);
 
     Player(Color color) {
@@ -19,8 +20,20 @@ public class Player {
 
     Point readPosition(String message) {
         System.out.println(message);
-        int column = input.nextInt();
-        int row = input.nextInt();
+        int row = 0;
+        int column = 0;
+
+        boolean IsInputNotNumerical = true;
+        while (IsInputNotNumerical) {
+            try {
+                column = input.nextInt();
+                row = input.nextInt();
+                IsInputNotNumerical = false;
+            } catch (InputMismatchException e) {
+                System.out.println("Please enter a valid expression");
+                input.next();
+            }
+        }
         return new Point(row - 1, column - 1);
     }
 
