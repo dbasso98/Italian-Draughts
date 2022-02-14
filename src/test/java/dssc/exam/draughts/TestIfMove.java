76f68e2e9374b83428df8ad85f09866ca7e2b58b
dbasso98 <dav.basso98@gmail.com>
2022-02-14 18:27:28 +0100

    Added IncompleteMoveException

--- src/test/java/dssc/exam/draughts/TestIfMove.java
+++ src/test/java/dssc/exam/draughts/TestIfMove.java
@@ -63,11 +63,11 @@ public class TestIfMove {
     @Test
     void suggestOptimalMoveEvenIfOriginalIsASkip() throws Exception{
         var newBoard = new Board();
-        newBoard.getPieceAtTile(2,1).upgradeToKing();
-        Move.movePiece(newBoard, new Point(5,4), new Point(3,2));
-        Move.movePiece(newBoard, new Point(6,1), new Point(5,4));
-        Move.movePiece(newBoard, new Point(6,5), new Point(3,6));
-        Exception exception = assertThrows(InvalidMoveException.class, () -> Move.moveDecider(newBoard, new Point(2,3), new Point(4,1)));
+        newBoard.getPieceAtTile(2, 1).upgradeToKing();
+        Move.movePiece(newBoard, new Point(5, 4), new Point(3, 2));
+        Move.movePiece(newBoard, new Point(6, 1), new Point(5, 4));
+        Move.movePiece(newBoard, new Point(6, 5), new Point(3, 6));
+        Exception exception = assertThrows(InvalidMoveException.class, () -> Move.moveDecider(newBoard, new Point(2, 3), new Point(4, 1)));
         assertEquals("You can select a better skip! Choose one of the tiles at these positions: (2,3) ", exception.getMessage());
     }
 }
