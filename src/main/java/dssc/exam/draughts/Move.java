638ae63a1d4154a895cd7c88484f249b277639fa
mibbim <michele.berti.g@gmail.com>
2022-02-15 11:09:31 +0100

    Refactored useless try and catch statements

--- src/main/java/dssc/exam/draughts/Move.java
+++ src/main/java/dssc/exam/draughts/Move.java
@@ -8,40 +8,34 @@ import java.util.Collections;
 import java.util.stream.*;
 
 public class Move {
-    public final Point source;
-    public final Point destination;
 
     public static void moveDecider(Board board, Point source, Point destination) throws Exception {
-        try {
-            MoveRules.checkIfPositionsAreValid(board, source, destination);
-            var candidateTiles = MoveRules.candidateTilesForSkipMove(board, board.getColorOfPieceAtTile(source));
-            int maxWeight;
-            if (!candidateTiles.isEmpty())
-                maxWeight = Collections.max(candidateTiles.values());
-            else
-                maxWeight = 0;
-            var bestTilesToStartTheSkip = new ArrayList<>(candidateTiles.entrySet().stream()
-                    .filter(entry -> entry.getValue() == maxWeight)
-                    .map(entry -> entry.getKey())
-                    .collect(Collectors.toList()));
+        MoveRules.checkIfPositionsAreValid(board, source, destination);
+        var candidateTiles = MoveRules.candidateTilesForSkipMove(board, board.getColorOfPieceAtTile(source));
+        int maxWeight;
+        if (!candidateTiles.isEmpty())
+            maxWeight = Collections.max(candidateTiles.values());
+        else
+            maxWeight = 0;
+        var bestTilesToStartTheSkip = new ArrayList<>(candidateTiles.entrySet().stream()
+                .filter(entry -> entry.getValue() == maxWeight)
+                .map(entry -> entry.getKey())
+                .collect(Collectors.toList()));
 
-            if (isASimpleMove(source, destination)) {
-                if (candidateTiles.isEmpty())
-                    diagonalMove(board, source, destination);
-                else
-                    throw new InvalidMoveException("There are pieces that must capture, try these positions: "
-                            + printPositionsOfTiles(bestTilesToStartTheSkip));
-            } else {
-                if (bestTilesToStartTheSkip.contains(board.getTile(source))) {
-                    skipMove(board, source, destination);
-                    if (candidateTiles.get(board.getTile(source)) > 1)
-                        throw new IncompleteMoveException("You can continue to skip!", candidateTiles.get(board.getTile(source)));
-                } else
-                    throw new InvalidMoveException("You can select a better skip! Choose one of the tiles at these positions: "
-                            + printPositionsOfTiles(bestTilesToStartTheSkip));
-            }
-        } catch (Exception e) {
-            throw e;
+        if (isASimpleMove(source, destination)) {
+            if (candidateTiles.isEmpty())
+                diagonalMove(board, source, destination);
+            else
+                throw new InvalidMoveException("There are pieces that must capture, try these positions: "
+                        + printPositionsOfTiles(bestTilesToStartTheSkip));
+        } else {
+            if (bestTilesToStartTheSkip.contains(board.getTile(source))) {
+                skipMove(board, source, destination);
+                if (candidateTiles.get(board.getTile(source)) > 1)
+                    throw new IncompleteMoveException("You can continue to skip!", candidateTiles.get(board.getTile(source)));
+            } else
+                throw new InvalidMoveException("You can select a better skip! Choose one of the tiles at these positions: "
+                        + printPositionsOfTiles(bestTilesToStartTheSkip));
         }
     }
 
@@ -62,39 +56,27 @@ public class Move {
     }
 
     static void skipMove(Board board, Point source, Point destination) throws Exception {
-        try {
-            var sourceTile = board.getTile(source);
-            var middleTile = board.getTile(board.getMiddlePosition(source, destination));
-            if (middleTile.isEmpty())
-                throw new EmptyTileException("Skip move over two empty tiles is not accepted");
-            if (middleTile.getPiece().getColor() == sourceTile.getPiece().getColor())
-                throw new SameColorException("Color of piece to skip cannot be the same as source piece");
-            diagonalMove(board, source, destination);
-            middleTile.popPiece();
-        } catch (Exception e) {
-            throw e;
-        }
+        var sourceTile = board.getTile(source);
+        var middleTile = board.getTile(board.getMiddlePosition(source, destination));
+        if (middleTile.isEmpty())
+            throw new EmptyTileException("Skip move over two empty tiles is not accepted");
+        if (middleTile.getPiece().getColor() == sourceTile.getPiece().getColor())
+            throw new SameColorException("Color of piece to skip cannot be the same as source piece");
+        diagonalMove(board, source, destination);
+        middleTile.popPiece();
     }
 
     public static void diagonalMove(Board board, Point source, Point destination) throws Exception {
-        try {
-            var sourceTile = board.getTile(source);
-            var destinationTile = board.getTile(destination);
-            MoveRules.checkTileEmptiness(source, sourceTile);
-            MoveRules.checkTileNonEmptiness(destination, destinationTile);
-            movePiece(sourceTile, destinationTile);
-        } catch (Exception e) {
-            throw e;
-        }
+        var sourceTile = board.getTile(source);
+        var destinationTile = board.getTile(destination);
+        MoveRules.checkTileEmptiness(source, sourceTile);
+        MoveRules.checkTileNonEmptiness(destination, destinationTile);
+        movePiece(sourceTile, destinationTile);
     }
 
     public static boolean isASimpleMove(Point source, Point destination) throws Exception {
-        try {
-            MoveRules.isDiagonal(source, destination);
-            return Math.abs(destination.x - source.x) == 1;
-        } catch (Exception e) {
-            throw e;
-        }
+        MoveRules.isDiagonal(source, destination);
+        return Math.abs(destination.x - source.x) == 1;
     }
 
     public static void movePiece(Tile sourceTile, Tile destinationTile) {
@@ -103,11 +85,7 @@ public class Move {
     }
 
     public static void movePiece(Board board, Point source, Point destination) throws Exception {
-        try {
-            movePiece(board.getTile(source), board.getTile(destination));
-        } catch (Exception e) {
-            throw e;
-        }
-    }
+        movePiece(board.getTile(source), board.getTile(destination));
 
+    }
 }
