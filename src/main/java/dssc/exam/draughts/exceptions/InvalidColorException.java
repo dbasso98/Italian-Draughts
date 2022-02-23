5e278314e76f8b9376e60ff0d38518236a55a9a0
theabm <andres_bermeo@outlook.com>
2022-02-23 11:48:06 +0100

    Deleted InvalidColorException

--- src/main/java/dssc/exam/draughts/exceptions/InvalidColorException.java
+++
@@ -1,8 +0,0 @@
-package dssc.exam.draughts.exceptions;
-
-public class InvalidColorException extends Exception {
-    public InvalidColorException(String message) {
-        super(message);
-    }
-
-}
