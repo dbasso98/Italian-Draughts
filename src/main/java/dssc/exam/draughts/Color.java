65a36cfca43379473c166e8241e036fe53a84c25
mibbim <michele.berti.g@gmail.com>
2022-02-25 14:16:54 +0100

    Reformat code

--- src/main/java/dssc/exam/draughts/Color.java
+++ src/main/java/dssc/exam/draughts/Color.java
@@ -1,19 +1,22 @@
 package dssc.exam.draughts;
 
 public enum Color {
-    BLACK(-1, 0), WHITE(1, BoardSpecifications.numberOfRows()-1);
+    BLACK(-1, 0),
+    WHITE(1, BoardSpecifications.numberOfRows() - 1);
 
-    private int direction;
-    private int endOfBoardIndex;
+    private final int direction;
+    private final int endOfBoardIndex;
 
-    private Color(int direction, int endOfBoardIndex){
+    Color(int direction, int endOfBoardIndex) {
         this.direction = direction;
         this.endOfBoardIndex = endOfBoardIndex;
     }
-    public int associatedDirection(){
+
+    public int associatedDirection() {
         return this.direction;
     }
-    public int associatedEndOfBoardRow(){
+
+    public int associatedEndOfBoardRow() {
         return this.endOfBoardIndex;
     }
 }
