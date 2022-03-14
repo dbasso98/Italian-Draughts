516be8080d717306047a94e1f5fdae6b85fe63ba
theabm <andres_bermeo@outlook.com>
2022-03-14 21:30:55 +0100

    migrated tests related to displaying of Board to a test class for the display of board

--- src/test/java/dssc/exam/draughts/TestIfBoard.java
+++ src/test/java/dssc/exam/draughts/TestIfBoard.java
@@ -1,16 +1,12 @@
 package dssc.exam.draughts;
 
-import dssc.exam.draughts.display.DisplayBoard;
 import net.jqwik.api.*;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.awt.*;
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -86,31 +82,4 @@ public class TestIfBoard {
     Arbitrary<Integer> validRowColumnGenerator () {
         return Arbitraries.integers().between(0, 7);
     }
-
-    @BeforeEach
-    private void makeBoardWithWhiteKingAndBlackKing() {
-        board.getPieceAtTile(1, 0).upgradeToKing();
-        board.getPieceAtTile(7, 4).upgradeToKing();
-    }
-
-    @Test
-    void printBoard() {
-        String expected = "   1  2  3  4  5  6  7  8" + System.lineSeparator() +
-                "8 [b][ ][b][ ][B][ ][b][ ] 8" + System.lineSeparator() +
-                "7 [ ][b][ ][b][ ][b][ ][b] 7" + System.lineSeparator() +
-                "6 [b][ ][b][ ][b][ ][b][ ] 6" + System.lineSeparator() +
-                "5 [ ][ ][ ][ ][ ][ ][ ][ ] 5" + System.lineSeparator() +
-                "4 [ ][ ][ ][ ][ ][ ][ ][ ] 4" + System.lineSeparator() +
-                "3 [ ][w][ ][w][ ][w][ ][w] 3" + System.lineSeparator() +
-                "2 [W][ ][w][ ][w][ ][w][ ] 2" + System.lineSeparator() +
-                "1 [ ][w][ ][w][ ][w][ ][w] 1" + System.lineSeparator() +
-                "   1  2  3  4  5  6  7  8" + System.lineSeparator();
-
-        ByteArrayOutputStream fakeStandardOutput = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(fakeStandardOutput));
-
-        new DisplayBoard().display(board);
-        assertEquals(expected, fakeStandardOutput.toString());
-    }
-
 }
---
+++ src/test/java/dssc/exam/draughts/display/TestIfDisplayBoard.java
@@ -0,0 +1,41 @@
+package dssc.exam.draughts.display;
+
+import dssc.exam.draughts.Board;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestIfDisplayBoard {
+    Board board = new Board();
+
+
+    @BeforeEach
+    private void makeBoardWithWhiteKingAndBlackKing() {
+        board.getPieceAtTile(1, 0).upgradeToKing();
+        board.getPieceAtTile(7, 4).upgradeToKing();
+    }
+
+    @Test
+    void printsBoardCorrectly() {
+        String expected = "   1  2  3  4  5  6  7  8" + System.lineSeparator() +
+                "8 [b][ ][b][ ][B][ ][b][ ] 8" + System.lineSeparator() +
+                "7 [ ][b][ ][b][ ][b][ ][b] 7" + System.lineSeparator() +
+                "6 [b][ ][b][ ][b][ ][b][ ] 6" + System.lineSeparator() +
+                "5 [ ][ ][ ][ ][ ][ ][ ][ ] 5" + System.lineSeparator() +
+                "4 [ ][ ][ ][ ][ ][ ][ ][ ] 4" + System.lineSeparator() +
+                "3 [ ][w][ ][w][ ][w][ ][w] 3" + System.lineSeparator() +
+                "2 [W][ ][w][ ][w][ ][w][ ] 2" + System.lineSeparator() +
+                "1 [ ][w][ ][w][ ][w][ ][w] 1" + System.lineSeparator() +
+                "   1  2  3  4  5  6  7  8" + System.lineSeparator();
+
+        ByteArrayOutputStream fakeStandardOutput = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(fakeStandardOutput));
+
+        new DisplayBoard().display(board);
+        assertEquals(expected, fakeStandardOutput.toString());
+    }
+}
