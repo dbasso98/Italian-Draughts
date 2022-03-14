c23ea319831f3e28584adcdb2e5eefc43d9b4389
theabm <andres_bermeo@outlook.com>
2022-03-14 21:48:42 +0100

    Introduced parameterized tests to check all cases of display of piece

--- src/test/java/dssc/exam/draughts/display/TestIfDisplayPiece.java
+++ src/test/java/dssc/exam/draughts/display/TestIfDisplayPiece.java
@@ -2,7 +2,8 @@ package dssc.exam.draughts.display;
 
 import dssc.exam.draughts.Color;
 import dssc.exam.draughts.Piece;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
@@ -10,15 +11,27 @@ import java.io.PrintStream;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TestIfDisplayPiece {
-    private static final String piece =  "[b]";
 
-    @Test
-    void printsToStdOutput() {
+    @ParameterizedTest
+    @CsvSource({"BLACK, [b]", "WHITE, [w]"})
+    void printsManToStdOutput(Color colorOfPiece, String representationOfPiece) {
         ByteArrayOutputStream fakeStandardOutput = new ByteArrayOutputStream();
         System.setOut(new PrintStream(fakeStandardOutput));
-        var fakePiece = new Piece(Color.BLACK);
-        fakePiece.display();
+        var fakePiece = new Piece(colorOfPiece);
 
-        assertEquals(TestIfDisplayPiece.piece, fakeStandardOutput.toString());
+        new DisplayPiece().display(fakePiece);
+        assertEquals(representationOfPiece, fakeStandardOutput.toString());
     }
+    @ParameterizedTest
+    @CsvSource({"BLACK, [B]", "WHITE, [W]"})
+    void printsKingToStdOutput(Color colorOfPiece, String representationOfPiece) {
+        ByteArrayOutputStream fakeStandardOutput = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(fakeStandardOutput));
+        var fakePiece = new Piece(colorOfPiece);
+        fakePiece.upgradeToKing();
+
+        new DisplayPiece().display(fakePiece);
+        assertEquals(representationOfPiece, fakeStandardOutput.toString());
+    }
+
 }
