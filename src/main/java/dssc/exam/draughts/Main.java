e4573f87258df52ae5d9d8f1d49fdbe45358375d
mibbim <michele.berti.g@gmail.com>
2022-03-22 11:57:09 +0100

    Cleaned Main

--- src/main/java/dssc/exam/draughts/Main.java
+++ src/main/java/dssc/exam/draughts/Main.java
@@ -1,14 +1,11 @@
 package dssc.exam.draughts;
 
 import dssc.exam.draughts.core.Game;
-import dssc.exam.draughts.core.Player;
 import dssc.exam.draughts.exceptions.DraughtsException;
-import dssc.exam.draughts.utilities.Color;
 
 public class Main {
     public static void main(String... args) throws DraughtsException {
         System.out.println("ciao!");
-        Player player = new Player(Color.WHITE);
         Game game = new Game();
         game.startGame();
     }
